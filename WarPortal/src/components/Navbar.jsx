549ccee801947bca9ddda93b4a1a2775eb37e22d
Susandinelli <susandinelli@gmail.com>
2022-10-28 09:33:07 -0500

    added basic nav to site

---
+++ WarPortal/src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+export default function Navbar() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/ContactUs">Contact Us</Link>
+          </li>
+          <li>
+            <Link to="/Team">Team With War Portal</Link>
+          </li>
+          <li>
+            <Link to="/StoryArcs">Story Arcs</Link>
+          </li>
+          <li>
+            <Link to="/Shop">Shop</Link>
+          </li>
+          <li>
+            <Link to="/Events">Events</Link>
+          </li>
+          <li>
+            <Link to="/Team">Team With War Portal</Link>
+          </li>
+          <li>
+            <Link to="/Account">Account</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}

c61c4462f22359f5e24f547a90ab638140c58c31
Susandinelli <susandinelli@gmail.com>
2022-11-08 13:22:36 -0500

    styled navbar yellow, rounded all corners

--- WarPortal/src/components/Navbar.jsx
+++ WarPortal/src/components/Navbar.jsx
@@ -7,40 +7,41 @@ export default function Navbar() {
     setOpen(!open);
   };
   return (
-    <div className="bg-yellow-900 text-white">
+    <div className="bg-yellow-500  text-yellow-900 border-8 border-white border-double m-4 p-6 rounded-full ">
       <nav>
-        <ul className="flex flex-wrap justify-center gap-8 text-center">
-          <li className="hover:bg-red-800 w-20 rounded-full ">
+        <ul className="flex flex-wrap gap-8 justify-center text-center place-items-center">
+          <li className="hover:bg-red-900 w-20  hover:bg-opacity-20 p-2 rounded-full font-bold hover:text-white hover:border-white text-center hover:border-2 hover:font-bold">
             <Link to="/Home">Home</Link>
           </li>
-          <li>
+          <li className="hover:bg-red-900 w-20  hover:bg-opacity-20 p-2 rounded-full font-bold hover:text-white hover:border-white text-center hover:border-2 hover:font-bold">
             <Link to="/Account">Account</Link>
           </li>
-          <li>
-            <Link to="/StoryArcs">Story Arcs</Link>
+          <li className="hover:bg-red-900 w-20  hover:bg-opacity-20 p-2 rounded-full font-bold hover:text-white hover:border-white text-center hover:border-2 hover:font-bold">           
+          
+           <Link to="/StoryArcs">Stories</Link>
           </li>
-          <li>
+          <li className="hover:bg-red-900 w-20  hover:bg-opacity-20 p-2 rounded-full font-bold hover:text-white hover:border-white text-center hover:border-2 hover:font-bold">
             <Link to="/Shop">Shop</Link>
           </li>
-          <li>
+          <li className="hover:bg-red-900 w-30  hover:bg-opacity-20 p-2 rounded-full font-bold hover:text-white hover:border-white text-center hover:border-2 hover:font-bold">
             <Link to="/ContactUs">Contact Us</Link>
           </li>
-          <button onClick={handleOpen}>Other</button>
+          <button className="hover:bg-red-900 w-20  hover:bg-opacity-20 p-2 rounded-full font-bold hover:text-white hover:border-white hover:border-2 hover:font-bold" onClick={handleOpen}>Other</button>
           {open ? (
             <div>
               <nav>
-                <ul className="flex flex-wrap justify-center gap-8 text-center">
-                  <li>
+                <ul className=" gap-8 ">
+                <li className="hover:bg-red-900 w-20 hover:bg-opacity-20 p-2 rounded-full font-bold hover:text-white hover:border-white text-center hover:border-2 hover:font-bold">
                     <Link to="/Team">Team With War Portal</Link>
                   </li>
 
-                  <li>
+                  <li className="hover:bg-red-900 w-20  hover:bg-opacity-20 p-2 rounded-full font-bold hover:text-white hover:border-white text-center hover:border-2 hover:font-bold">
                     <Link to="/OtherCreativeWorks">Other Creative Works</Link>
                   </li>
-                  <li>
+                  <li className="hover:bg-red-900 w-20  hover:bg-opacity-20 p-2 rounded-full font-bold hover:text-white hover:border-white text-center hover:border-2 hover:font-bold">
                     <Link to="/Events">Events</Link>
                   </li>
-                  <li>
+                  <li className="hover:bg-red-900 w-20  hover:bg-opacity-20 p-2 rounded-full font-bold hover:text-white hover:border-white text-center hover:border-2 hover:font-bold">
                     <Link to="/Investing">Investing</Link>
                   </li>
                 </ul>

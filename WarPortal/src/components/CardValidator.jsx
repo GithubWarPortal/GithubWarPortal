ecb3b9cab8e78c430792b1706d66c8fcb98924e2
Susandinelli <susandinelli@gmail.com>
2022-12-07 14:20:59 -0500

    made input required + reset + submit form buttons

--- WarPortal/src/components/CardValidator.jsx
+++ WarPortal/src/components/CardValidator.jsx
@@ -1,12 +1,15 @@
 export default function CardValidator() {
+  
     return (
         <>
           <h1>Card Validator</h1>
           <form>
             <h2>User ID</h2>
-            <input type="text" placeholder="Enter Here"></input>
+            <input type="text" placeholder="Enter Here" required></input>
             <h2>Card ID</h2>
-            <input type="text" placeholder="Enter Here"></input>
+            <input type="text" placeholder="Enter Here" required></input>
+           <button> <input type="reset"></input></button>
+           <button><input type="submit"></input> </button>
           </form>
         </>
       );

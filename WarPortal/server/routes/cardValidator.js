9c72892523973f58c53db33bb27460963984b8c9
Susandinelli <susandinelli@gmail.com>
2023-01-20 11:41:52 -0500

    made clear line in code between client and server

--- WarPortal/server/routes/cardValidator.js
+++ WarPortal/server/routes/cardValidator.js
@@ -3,14 +3,14 @@ const prisma = new PrismaClient();
 import express from "express";
 
 
-export default function CardValidatorAPI() {
+
 const app = express();
 app.use(express.json());
 
 
-app.get("/CardValidator", async (req, res) => {
+app.get("/Validator", async (req, res) => {
     const userCard = await prisma.usersCards.findFirstOrThrow({
       where: { userId, characterId },
     });
   
-    res.json(userCard)});}
+    res.json(userCard)});
--- WarPortal/server/server.js
+++ WarPortal/server/server.js
@@ -1,10 +1,18 @@
 import express from "express";
+import "./routes/cardValidator"
+
+const cors = require("cors");
+require("dotenv").config({ path: "./env" });
+const port = process.env.PORT || 5000;
+
 
 const app = express();
-app.use(require("./routes/cardValidator"));
+
 app.use(express.json());
+app.use(cors());
+
+app.use(require("./routes/cardValidator"));
 
 
-const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(port, () => console.log(`Server is running on port ${port}`));

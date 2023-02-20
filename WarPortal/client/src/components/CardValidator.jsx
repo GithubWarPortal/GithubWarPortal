84cafd5f9764c997a36ee290e788a10a78456a7d
Susandinelli <susandinelli@gmail.com>
2023-02-20 17:04:06 -0500

    added labels and semantic html elements

--- WarPortal/client/src/components/CardValidator.jsx
+++ WarPortal/client/src/components/CardValidator.jsx
@@ -36,21 +36,24 @@ export default function CardValidator() {
   };
   return (
     <>
-      <div class="mt-2 mb-2 flex items-center justify-center text-center text-lg text-red-800">
+      <div class="    mt-6 flex items-center justify-center text-center text-lg text-red-800">
         <form onSubmit={handleSubmit} class="">
-          <div class="flex justify-center">
+          <header class=" flex justify-center">
             {" "}
-            <h1 class="text flex w-2/3 justify-center rounded-full  bg-white bg-gradient-to-tr from-yellow-500  via-red-800 to-yellow-500 p-2 text-center align-middle text-white ">
+            <h1 class="  text flex w-2/3 justify-center rounded-full  bg-white bg-gradient-to-tr from-yellow-500  via-red-800 to-yellow-500 p-2 text-center align-middle text-white ">
               Card Validator
             </h1>
-          </div>
-          <div class="flex justify-center">
+          </header>
+
+          <section class=" flex justify-center">
+          <label for="User Id">
+
             {" "}
-            <h2 class="mt-2 w-1/3 rounded-full bg-gradient-to-tl from-yellow-500  via-red-800   to-yellow-500 p-1  text-white  hover:to-yellow-500">
+            <h2 class=" mt-2 w-1/3 rounded-full bg-gradient-to-tl from-yellow-500  via-red-800   to-yellow-500 p-1  text-white  hover:to-yellow-500">
               User ID
-            </h2>
-          </div>
-          <div class="mt-2  rounded-full  bg-gradient-to-tl   from-yellow-500 via-red-800 to-yellow-500 p-1 hover:to-yellow-500 ">
+            </h2></label>
+          </section>
+          <section class="  mt-2  rounded-full  bg-gradient-to-tl   from-yellow-500 via-red-800 to-yellow-500 p-1 hover:to-yellow-500 ">
             <input
               class="rounded-full bg-white p-2 text-center placeholder-red-800"
               type="text"
@@ -60,38 +63,45 @@ export default function CardValidator() {
               onChange={handleChange}
               required
             ></input>
-          </div>
+          </section>
 
-          <div class="flex justify-center">
+          <section class="  flex justify-center">
             {" "}
-            <h2 class="mt-2 w-1/3 rounded-full bg-gradient-to-tl from-yellow-500  via-red-800   to-yellow-500 p-1  text-white  hover:to-yellow-500">
+            <h2 class=" mt-2 w-1/3 rounded-full bg-gradient-to-tl from-yellow-500  via-red-800   to-yellow-500 p-1  text-white  hover:to-yellow-500">
               Card ID
             </h2>
-          </div>
-          <div class="mt-2  rounded-full  bg-gradient-to-tl   from-yellow-500 via-red-800 to-yellow-500 p-1 hover:to-yellow-500 ">
-            <input
-              class="rounded-full bg-white p-2 text-center placeholder-red-800"
+          </section>
+          <section class="   mt-2  rounded-full  bg-gradient-to-tl   from-yellow-500 via-red-800 to-yellow-500 p-1 hover:to-yellow-500 ">
+            <label for="Character Id">
+              <input
+              class=" rounded-full bg-white p-2 text-center placeholder-red-800"
               type="text"
-              name="characterId"
-              placeholder="Enter Here"
-              value={userInput.characterId}
-              onChange={handleChange}
-              required
-            ></input>
-          </div>
-
-          <input
-            type="submit"
-            class=" m-2 mt-2 w-1/3 rounded-full bg-gradient-to-tr  from-yellow-500   via-red-800  to-yellow-500 p-1 text-white hover:from-red-800 hover:via-yellow-500 hover:to-red-800"
-          ></input>
-          <h1 class=" text-red-800">Card is:</h1>
-          <div class="flex justify-center">
-            {" "}
-            <p class="flex w-1/3 justify-center  rounded-full  bg-gradient-to-tr  from-yellow-500   via-red-800 to-yellow-500 p-1 text-center align-middle text-white ">
+                name="characterId"
+                placeholder="Enter Here"
+                value={userInput.characterId}
+                onChange={handleChange}
+                required
+              ></input>
+            </label>
+          </section>
+          <section class="  ">
+            <label for="Submit Button">
+              <input
+                type="submit"
+                class=" m-2 mt-2 w-1/3 rounded-full bg-gradient-to-tr  from-yellow-500   via-red-800  to-yellow-500 p-1 text-white hover:from-red-800 hover:via-yellow-500 hover:to-red-800"
+              ></input>
+            </label>
+            <label for="Result">
+              <h3 class=" text-red-800">Card is:</h3>
+            </label>
+            <div class="flex justify-center">
               {" "}
-              {responseData}
-            </p>
-          </div>
+              <p class="flex w-1/3 justify-center  rounded-full  bg-gradient-to-tr  from-yellow-500   via-red-800 to-yellow-500 p-1 text-center align-middle text-white ">
+                {" "}
+                {responseData}
+              </p>
+            </div>
+          </section>
         </form>
       </div>
     </>

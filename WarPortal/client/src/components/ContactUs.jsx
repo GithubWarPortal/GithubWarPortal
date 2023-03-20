b1e9eafe852cea2d28d038b7f6608a66670271e8
Susandinelli <susandinelli@gmail.com>
2023-03-20 08:38:02 -0500

    Changed wording to be consistent on contact page

--- WarPortal/client/src/components/ContactUs.jsx
+++ WarPortal/client/src/components/ContactUs.jsx
@@ -11,7 +11,7 @@ export function ContactUs() {
         </h2>
         <button class="lg:p-4 mt-2 lg:m-10 lg:text-3xl lg:w-1/6 w-1/3 m-auto rounded-full bg-gradient-to-tr  from-yellow-500   via-red-800  to-yellow-500 p-1 text-white hover:from-red-800 hover:via-yellow-500 hover:to-red-800">
           <a class="mt-3" href="mailto:warportalonline@gmail.com">
-            Click Here to Email Me!
+            Click Here to Email Us!
           </a>
         </button>
       </div>

40bba5aab8837a9c08aad2705fb1548a7396d6e0
Susandinelli <susandinelli@gmail.com>
2023-04-06 15:33:03 -0500

    created structure of creator sign in page

--- WarPortal/client/src/components/SignIn/Creator.jsx
+++ WarPortal/client/src/components/SignIn/Creator.jsx
@@ -1,8 +1,33 @@
 export default function CreatorSignIn() {
+    //useState
+
+    //Frontend API
   return (
     <>
-      {" "}
-      <h1>Creator</h1>
+      <section>
+        <form>
+          <h1>Sign In</h1>
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email Here..."
+           // value={userInput.email}
+            //handle change
+            required
+          ></input>
+          <label>Password</label>
+          <input
+            type="text"
+            name="password"
+            placeholder="Password Here..."
+          //  value={userInput.password}
+            //handle change
+            required
+          ></input>                                                                                                                                                                                    
+          <input type="submit"></input>
+        </form>
+      </section>
     </>
   );
 }

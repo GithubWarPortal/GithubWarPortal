c98f09307cad876ca9b70d819bd669e6ec4bec3f
Susandinelli <susandinelli@gmail.com>
2023-04-11 12:56:09 -0500

    added stripe and creator and store backend APIs

--- WarPortal/server/server.js
+++ WarPortal/server/server.js
@@ -7,13 +7,16 @@ import storeSubmissionRouter from "./routes/CardCreator/storeSubmission.js";
 import creatorSubmissionRouter from "./routes/CardCreator/creatorSubmission.js";
 import creatorSignInRouter from "./routes/SignIn/creator.js";
 import storeSignInRouter from "./routes/SignIn/store.js";
-
+import stripeCreatorRouter from "./routes/Stripe/creator.js";
+import stripeStoreRouter from "./routes/Stripe/store.js";
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 //Router for card validator 
 app.use("/CardValidator", cardValidatorRouter);
@@ -23,6 +26,16 @@ app.use("/StoreSubmission", storeSubmissionRouter);
 app.use("/CreatorSubmission", creatorSubmissionRouter);
 app.use("/CreatorSignIn", creatorSignInRouter);
 app.use("/StoreSignIn", storeSignInRouter);
-//app.use("/StripeCreator", stripeCreatorRouter);
-//app.use("/StripeStore", stripeStoreRouter);
+
+//Router for creator subscriptions
+app.use("/create-creator-checkout-session", stripeCreatorRouter);
+app.use("/create-creator-portal-session", stripeCreatorRouter);
+app.use("/webhook-creator", stripeCreatorRouter);
+
+//Router for store subscriptions
+app.use("/create-creator-checkout-session", stripeStoreRouter);
+app.use("/create-creator-portal-session", stripeStoreRouter);
+app.use("/webhook-creator", stripeStoreRouter);
+
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));

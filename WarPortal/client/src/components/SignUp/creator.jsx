a33cdd4383c2cbccc580ecd058060b501139271d
Susandinelli <susandinelli@gmail.com>
2023-04-13 12:40:15 -0500

    created front end API for creator sign up

--- WarPortal/client/src/components/SignUp/creator.jsx
+++ WarPortal/client/src/components/SignUp/creator.jsx
@@ -1,16 +1,55 @@
-import CreatorStripe from "../Stripe/creator.jsx";
-
+import { useState } from "react";
 export default function CreatorSignUp() {
+  const [userInput, setUserInput] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+  const handleSubmit = async (req, res) => {
+    const creator = await fetch("http://localhost:5000/createCreator", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        firstName: userInput.firstName,
+        lastName: userInput.lastName,
+        email: userInput.email,
+        password: userInput.password,
+      }),
+    });
+  };
+  const handleChange = (event) => {
+    setUserInput({ ...userInput, [event.target.name]: event.target.value });
+  };
   return (
     <>
       <section>
-       <form> 
-          <label for="First Name">First Name</label><input type="text"></input>
-          <label for="Last Name">Last Name</label><input type="text"></input>
+        <form onSubmit={handleSubmit}>
+          <label for="First Name">First Name</label>
+          <input
+            type="text"
+            value={userInput.firstName}
+            onChange={handleChange}
+          ></input>
+          <label for="Last Name">Last Name</label>
+          <input
+            type="text"
+            value={userInput.lastName}
+            onChange={handleChange}
+          ></input>
           <label for="Email">Email</label>
-          <input type="email"></input>
-          <label for="Password">Password</label><input type="text"></input>
-        
+          <input
+            type="email"
+            value={userInput.email}
+            onChange={handleChange}
+          ></input>
+          <label for="Password">Password</label>
+          <input
+            type="text"
+            value={userInput.password}
+            onChange={handleChange}
+          ></input>
+          <input type="submit"></input>
         </form>
       </section>
     </>

2c7cec27234cfadea624e58e89a187c1eea30d4e
Susandinelli <susandinelli@gmail.com>
2023-04-16 13:40:48 -0500

    worked on sign up
    front and backend API's created as well as schema changes and routing

--- WarPortal/client/src/components/SignUp/creator.jsx
+++ WarPortal/client/src/components/SignUp/creator.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 export default function CreatorSignUp() {
   const [userInput, setUserInput] = useState({
     firstName: "",
     lastName: "",
     email: "",
     password: "",
+    creatorId: "",
   });
   const handleSubmit = async (req, res) => {
-    const creator = await fetch("http://localhost:5000/createCreator", {
+    const creator = await fetch("http://localhost:5000/CreateCreator", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -15,8 +17,12 @@ export default function CreatorSignUp() {
         lastName: userInput.lastName,
         email: userInput.email,
         password: userInput.password,
+        userId: userInput.userId,
       }),
     });
+    console.log(creator)
+  alert("Thank you for signing up!")
+    useNavigate("/SignUp");
   };
   const handleChange = (event) => {
     setUserInput({ ...userInput, [event.target.name]: event.target.value });
@@ -28,25 +34,40 @@ export default function CreatorSignUp() {
           <label for="First Name">First Name</label>
           <input
             type="text"
+            name="firstName"
             value={userInput.firstName}
             onChange={handleChange}
+            required
           ></input>
           <label for="Last Name">Last Name</label>
           <input
             type="text"
+            name="lastName"
             value={userInput.lastName}
             onChange={handleChange}
+            required
           ></input>
           <label for="Email">Email</label>
           <input
             type="email"
+            name="email"
             value={userInput.email}
             onChange={handleChange}
+            required
           ></input>
           <label for="Password">Password</label>
           <input
             type="text"
+            name="password"
             value={userInput.password}
+            onChange={handleChange}
+            required
+          ></input>
+          <label for="User Id">User ID (If you have a previous account)</label>
+          <input
+            type="text"
+            name="userId"
+            value={userInput.userId}
             onChange={handleChange}
           ></input>
           <input type="submit"></input>
